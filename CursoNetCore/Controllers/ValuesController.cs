44dd3931a6c343f14f284b0bd0af0b7a92773c55
carlosedugoc <carlosedugoc@gmail.com>
2019-03-14 16:29:00 -0500

    Se suben cambios

--- CursoNetCore/Controllers/ValuesController.cs
+++ CursoNetCore/Controllers/ValuesController.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CursoNetCore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("PermitirApiRequest")]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ValuesController : ControllerBase
     {
+        private readonly IDataProtector _protector;
+        private readonly HashService _hashService;
+
+        public ValuesController(IDataProtectionProvider protectionProvider)
+        {
+            _protector = protectionProvider.CreateProtector("valor_unico_y_quizas_secreto");
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
@@ -17,11 +31,26 @@ namespace CursoNetCore.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        [HttpGet("hash")]
+        public ActionResult GetHash()
+        {
+            string textoPlano = "Carlos";
+            var hashResult1 = _hashService.Hash(textoPlano).Hash;
+            var hashResult2 = _hashService.Hash(textoPlano).Hash;
+            return Ok(new { textoPlano, hashResult1, hashResult2 });
+        }
+
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            var protectorLimitadoPorTiempo = _protector.ToTimeLimitedDataProtector();
+            string textoPlano = "Carlos";
+            //string textoCifrado = _protector.Protect(textoPlano, TimeSpan);
+            string textoCifrado = protectorLimitadoPorTiempo.Protect(textoPlano, TimeSpan.FromSeconds(5));
+            Thread.Sleep(6000);
+            string textoDesencriptado = _protector.Unprotect(textoCifrado);
+            return Ok(new { textoPlano, textoCifrado, textoDesencriptado });
         }
 
         // POST api/values
--- CursoNetCore/Startup.cs
+++ CursoNetCore/Startup.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using CursoNetCore.Contexts;
 using CursoNetCore.Entities;
 using CursoNetCore.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CursoNetCore
 {
@@ -30,11 +34,39 @@ namespace CursoNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<HashService>();
+            services.AddDataProtection();
+
+            services.AddCors(options => 
+            options.AddPolicy("PermitirApiRequest",
+                builder => builder.WithOrigins("http://www.apirequest.io").WithMethods("GET", "POST").AllowAnyHeader()));
+            services.AddDataProtection();
+            services.AddDbContext<ApplicationIdentityDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddAutoMapper(options=>options.CreateMap<AutorCreacionDTO, Autor>());
+
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddJsonOptions(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore );
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(Configuration["JWT:Key"])),
+                ClockSkew = TimeSpan.Zero
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,8 +81,13 @@ namespace CursoNetCore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
+            app.UseCors();
+            //app.UseCors(builder => builder
+            //                .WithOrigins("http://apirequest.io/")
+            //                .WithMethods("GET", "POST")
+            //                .WithHeaders("*"));
             app.UseMvc();
         }
     }
